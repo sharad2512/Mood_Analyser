f28951edeaf0efcc974b1c5bd0e02e2cfb89e640
sharad2512 <shnarute95@gmail.com>
2023-01-17 13:54:31 +0530

    ADDED : Code of To Handle Exception If User Provides Invalid Mood

--- src/main/java/com/bridgelab/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelab/moodanalyser/MoodAnalyser.java
@@ -4,15 +4,23 @@ public class MoodAnalyser {
     String message;
 
     public MoodAnalyser() {
-
     }
+
     public MoodAnalyser(String message) {
         this.message = message;
     }
+
     public String analyseMood(String message) {
-        if (message.contains("Sad")) {
-            return "SAD";
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            }
+        } catch (NullPointerException ex) {
+            return "HAPPY";
         }
         return "HAPPY";
+    }
+    public String analyseMood() {
+        return analyseMood(this.message);
     }
 }
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,23 +1,24 @@
 package com.bridgelabz.moodanalyser;
-
 import com.bridgelab.moodanalyser.MoodAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
-
-
 public class MoodAnalyserTest {
-
     @Test
     public void givenMessage_whenSad_shouldReturnSadMood() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I Am In Sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In Sad Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
-
     @Test
     public void givenMessage_whenSad_shouldReturnHappyMood() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I Am In Happy Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In Happy Mood");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+    @Test
+    public void givenMessage_whenNull_shouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
 }
