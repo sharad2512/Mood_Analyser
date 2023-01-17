eae1f56b30b4fc0bb90b9214e62774c40976d9f6
sharad2512 <shnarute95@gmail.com>
2023-01-17 13:44:57 +0530

    ADDED : Refctor UC1

--- src/main/java/com/bridgelab/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelab/moodanalyser/MoodAnalyser.java
@@ -1,11 +1,18 @@
 package com.bridgelab.moodanalyser;
 
 public class MoodAnalyser {
+    String message;
+
+    public MoodAnalyser() {
+
+    }
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
     public String analyseMood(String message) {
         if (message.contains("Sad")) {
             return "SAD";
         }
         return "HAPPY";
     }
-
 }
